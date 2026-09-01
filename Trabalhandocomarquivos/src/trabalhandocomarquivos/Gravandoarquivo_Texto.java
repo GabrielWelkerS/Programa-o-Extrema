2beb3828eb688b4b4f50730eee4178e142b59d6e
Gabriel Welker <gabriel.welkersilva@gmail.com>
2026-09-01 06:48:31 -0300

    update
    FileWrite

---
+++ Trabalhandocomarquivos/src/trabalhandocomarquivos/Gravandoarquivo_Texto.java
@@ -0,0 +1,53 @@
+package trabalhandocomarquivos;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class Gravandoarquivo_Texto {
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner teclado = new Scanner(System.in);
+
+        String matricula;
+        String nome;
+        double teste, prova;
+        int faltas;
+
+        FileWriter arq = new FileWriter(
+            "/home/gabriel/Documentos/trabalhando_arquivos/exemplo1.txt"
+        );
+
+        PrintWriter gravarArq = new PrintWriter(arq);
+
+        System.out.println("Digite sua matricula: ");
+        matricula = teclado.next();
+
+        teclado.nextLine();
+
+        System.out.println("Digite o seu nome: ");
+        nome = teclado.nextLine();
+
+        System.out.println("Digite seu teste: ");
+        teste = teclado.nextDouble();
+
+        System.out.println("Digite sua prova: ");
+        prova = teclado.nextDouble();
+
+        System.out.println("Digite suas faltas: ");
+        faltas = teclado.nextInt();
+
+        gravarArq.println(matricula);
+        gravarArq.println(nome);
+        gravarArq.println(teste);
+        gravarArq.println(prova);
+        gravarArq.println(faltas);
+
+        arq.close();
+        teclado.close();
+
+        System.out.println("Gravação efetuada com sucesso");
+    }
+}
---
+++ Trabalhandocomarquivos/src/trabalhandocomarquivos/Gravandoarquivo_binario.java
@@ -0,0 +1,53 @@
+package trabalhandocomarquivos;
+
+import java.io.DataOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Gravandoarquivo_binario {
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner teclado = new Scanner(System.in);
+
+        String matricula;
+        String nome;
+        double teste, prova;
+        int faltas;
+
+        FileOutputStream arq = new FileOutputStream(
+            "/home/gabriel/Documentos/trabalhando_arquivos/exemplo1.txt"
+        );
+
+        DataOutputStream gravarArq = new DataOutputStream(arq);
+
+        System.out.println("Digite sua matricula: ");
+        matricula = teclado.next();
+
+        teclado.nextLine();
+
+        System.out.println("Digite o seu nome: ");
+        nome = teclado.nextLine();
+
+        System.out.println("Digite seu teste: ");
+        teste = teclado.nextDouble();
+
+        System.out.println("Digite sua prova: ");
+        prova = teclado.nextDouble();
+
+        System.out.println("Digite suas faltas: ");
+        faltas = teclado.nextInt();
+
+        gravarArq.writeUTF(matricula);
+        gravarArq.writeUTF(nome);
+        gravarArq.writeDouble(teste);
+        gravarArq.writeDouble(prova);
+        gravarArq.writeInt(faltas);
+
+        gravarArq.close();
+        teclado.close();
+
+        System.out.println("Gravação efetuada com sucesso");
+    }
+}
